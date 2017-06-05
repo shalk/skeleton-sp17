1b182356a1cabf48d013d6db4bf868c70282f81f
shalk <xshalk@163.com>
2017-06-05 22:22:49 +0800

    implement  mehtod  isLeapYear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -3,6 +3,13 @@
  */
 public class LeapYear {
 
+	private static boolean isLeapYear(int year) {
+		if ( year % 4 != 0 )
+			return false;
+		if ( year % 100 == 0 && year % 400 != 0 )
+            return false;
+		return true;
+	}
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
